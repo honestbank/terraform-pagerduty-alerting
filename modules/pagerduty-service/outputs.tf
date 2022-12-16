ac2a1771d6ec8fb1a48901232d0d73b0620bda27
Nishchal Gautam <nishchal@honestbank.com>
2022-12-16 10:25:40 +0700

    feat: add output for service name

--- modules/pagerduty-service/outputs.tf
+++ modules/pagerduty-service/outputs.tf
@@ -2,3 +2,7 @@ output "id" {
   description = "The `id` attribute of the service."
   value       = pagerduty_service.service.id
 }
+
+output "service_name" {
+  value = pagerduty_service.service.name
+}
